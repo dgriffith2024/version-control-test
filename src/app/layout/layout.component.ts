ee9c423ee1643e2dba296fc8ae10679b33111f78
dgriffith2024 <dgriffith2024@users.noreply.github.com>
2024-09-11 05:26:43 +0000

    Apply formatting changes

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -4,26 +4,19 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent implements OnInit {
   spinner: boolean = false;
   usuario: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  promCrearReferidos(formPromReferidos: NgForm) {
+    console.log('usuario: ', this.usuario);
+
+    console.log('formPromReferidos value: ', formPromReferidos.value);
   }
-
-  promCrearReferidos(formPromReferidos: NgForm){
-
-    console.log('usuario: ', this.usuario)
-
-
-
-
-
-                  console.log('formPromReferidos value: ', formPromReferidos.value)
-  }
-
 }
